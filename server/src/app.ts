96816172fc38e219fbbf589690ca10140a44d297
acf-patrick <miharisoap@gmail.com>
2023-02-01 21:17:14 +0300

    add Express

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hello world")
+import { Request, Response } from "express";
+
+const express = require("express");
+
+const app = express();
+
+const PORT = 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  const { name } = req.query;
+  res.send(`Hello ${name}!`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
