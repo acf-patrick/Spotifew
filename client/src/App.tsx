ca74047687fdcf6aadd812f828eb53eb1ca71b09
acf-patrick <miharisoap@gmail.com>
2023-02-02 21:21:03 +0300

    restructure project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const accessToken = params.get("access_token");
+    const refreshToken = params.get("refresh_token");
+
+    if (refreshToken) {
+      fetch(`/api/refresh_token?refresh_token=${refreshToken}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    }
+  }, []);
 
   return (
     <div className="App">
       <a href="http://localhost:8000/login">Log in to Spotify</a>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
