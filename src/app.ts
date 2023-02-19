f8d4e74044e9fe2b3fecf5d5ab56daf042fe7315
acf-patrick <miharisoap@gmail.com>
2023-02-19 21:51:52 +0300

    add FRONTEND_URI to env

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ const app = express();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
 const PORT = process.env.PORT || 8000;
 
 const stateKey = "spotify_auth_state";
@@ -74,7 +75,7 @@ app.get("/callback", (req: Request, res: Response) => {
           refresh_token,
           expires_in
         });
-        res.redirect(`http://localhost:3000/?${queryParams}`);
+        res.redirect(`${FRONTEND_URI}/?${queryParams}`);
       } else {
         res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
       }
