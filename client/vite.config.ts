96ec6ecd8f303c1c686e3b56e542d2c8630e6f1b
acf-patrick <miharisoap@gmail.com>
2023-02-20 20:07:50 +0300

    set client proxy for dev and prod

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,18 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Spotifew",
-  server: {
-    host: true,
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    base: "/Spotifew",
+    server: {
+      host: true,
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: mode === "production" ? "https://spotifew.cyclic.app" : "http://localhost:8000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
