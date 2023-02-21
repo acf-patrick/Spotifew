b71f7069f18b131fb7ad8dd3069b3b7e4aaa8b7a
acf-patrick <miharisoap@gmail.com>
2023-02-21 11:32:30 +0300

    remove base path from vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react()],
-    base: "/Spotifew",
     server: {
       host: true,
       port: 3000,
